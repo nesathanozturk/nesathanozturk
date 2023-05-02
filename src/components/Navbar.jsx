b5c19d9e636f3d7dc8305c283f88f0768ddd1801
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-02 12:36:05 +0300

    feat: adde darkmode icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import { MdDarkMode } from "react-icons/md";
+
 const Navbar = () => {
   return (
     <nav className="w-full py-4 px-16 flex justify-between items-center">
-      <h1 className="font-caveat text-3xl hover:text-purple-700 transition-colors cursor-pointer">
+      <h1 className="font-caveat text-3xl text-purple-700 transition-colors cursor-pointer">
         Neşathan Öztürk
       </h1>
       <div>
@@ -17,6 +19,9 @@ const Navbar = () => {
           </li>
           <li className="hover:text-purple-700 transition-colors">
             <a href="#projects">Projects</a>
+          </li>
+          <li className="cursor-pointer hover:text-gray-800 transition-colors">
+            <MdDarkMode size={25} />
           </li>
         </ul>
       </div>
