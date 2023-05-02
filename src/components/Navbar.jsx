2be93bd70fa8034baa0f2e1a5ce0cd1a18238bd9
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-02 12:32:23 +0300

    style: edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,27 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full py-4 px-16 flex justify-between items-center">
+      <h1 className="font-caveat text-3xl hover:text-purple-700 transition-colors cursor-pointer">
+        Neşathan Öztürk
+      </h1>
+      <div>
+        <ul className="flex justify-center items-center gap-5 font-semibold">
+          <li className="hover:text-purple-700 transition-colors">
+            <a href="#home">Home</a>
+          </li>
+          <li className="hover:text-purple-700 transition-colors">
+            <a href="#about">About</a>
+          </li>
+          <li className="hover:text-purple-700 transition-colors">
+            <a href="#skills">Skills</a>
+          </li>
+          <li className="hover:text-purple-700 transition-colors">
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
