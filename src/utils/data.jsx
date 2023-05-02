e7e4575351492d50b42e714b8affb3c300daa481
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-02 16:47:00 +0300

    feat: created projects data

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -4,3 +4,30 @@ export const home = [
     job: "A FRONTEND DEVELOPER.",
   },
 ];
+
+export const projects = [
+  {
+    id: 1,
+    title: "findmovie-app",
+    language: "Javascript",
+    star: 1,
+  },
+  {
+    id: 2,
+    title: "getir-clone",
+    language: "Javascript",
+    star: 2,
+  },
+  {
+    id: 3,
+    title: "simple-e-commerce",
+    language: "Javascript",
+    star: 0,
+  },
+  {
+    id: 4,
+    title: "react-netflix-clone",
+    language: "Javascript",
+    star: 1,
+  },
+];
