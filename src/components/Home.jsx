c7b91ff360b4eb6702aa1c2ba27c482591cbb3d2
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-05-02 12:46:00 +0300

    feat: added typewriter effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,27 @@
+import Navbar from "./Navbar";
+import Typewriter from "typewriter-effect";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <section>
+        <div className="container">
+          <div className="left">
+            <h2>Hey There &#128075;</h2>
+
+            <Typewriter
+              options={{
+                strings: ["Hello", "World"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
