b855863e3b236d0af9933c0e2c23dc0d1b7c69b3
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-11 13:20:13 +0300

    style: edited btn

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -37,21 +37,12 @@ const Hero = ({ dark }) => {
               />
             </a>
           </div>
-          {dark ? (
-            <a
-              href="mailto:nesathanozturk061@gmail.com"
-              className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            >
-              Contact Me
-            </a>
-          ) : (
-            <a
-              href="mailto:nesathanozturk061@gmail.com"
-              className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-            >
-              Contact Me
-            </a>
-          )}
+          <a
+            href="mailto:nesathanozturk061@gmail.com"
+            className="from-purple-500 via-purple-600 to-purple-700 focus:ring-purple-300 dark:focus:ring-purple-800 shadow-purple-500/50 dark:shadow-purple-800/80 text-white bg-gradient-to-r hover:bg-gradient-to-br focus:ring-4 focus:outline-none shadow-lg dark:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:darkBtn"
+          >
+            Contact Me
+          </a>
         </div>
         <div className="w-full md:w-1/4">
           <img
